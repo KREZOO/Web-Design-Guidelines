81d3bb8fd2f1508d4018c7239376da3eed2da281
KREZOO <krezoo.yt@gmail.com>
2023-12-18 14:35:01 +0200

    save fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,42 @@ function getPages() {
     return input;
 }
 
+function copyMaterials() {
+    // Копируем содержимое папки materials в dist
+    const materialsDir = resolve(process.cwd(), 'materials');
+    const distMaterialsDir = resolve(process.cwd(), 'dist/materials');
+
+    // Создаем dist/materials, если его нет
+    if (!fs.existsSync(distMaterialsDir)) {
+        fs.mkdirSync(distMaterialsDir);
+    }
+
+    // Копируем все файлы из materials
+    const files = fs.readdirSync(materialsDir);
+    for (const file of files) {
+        const sourcePath = resolve(materialsDir, file);
+        const destPath = resolve(distMaterialsDir, file);
+
+        // Если это директория, создаем ее в dist/materials
+        if (fs.statSync(sourcePath).isDirectory()) {
+            if (!fs.existsSync(destPath)) {
+                fs.mkdirSync(destPath);
+            }
+
+            // Копируем содержимое директории
+            const filesInDir = fs.readdirSync(sourcePath);
+            for (const fileInDir of filesInDir) {
+                const sourceFilePath = resolve(sourcePath, fileInDir);
+                const destFilePath = resolve(destPath, fileInDir);
+                fs.copyFileSync(sourceFilePath, destFilePath);
+            }
+        } else {
+            // Если это файл, просто копируем
+            fs.copyFileSync(sourcePath, destPath);
+        }
+    }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/Web-Design-Guidelines/",
@@ -32,4 +68,12 @@ export default defineConfig({
             input: getPages(),
         },
     },
+    plugins: [
+        {
+            name: 'copy-materials',
+            writeBundle() {
+                copyMaterials();
+            },
+        },
+    ],
 })
